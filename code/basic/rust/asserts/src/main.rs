f2d6f3f93b40997a0db66be898fbac1830259b03
jaylen.chan <jaylen.chan@hotmail.com>
2022-01-30 18:20:48 +0800

    code(rust)

---
+++ code/basic/rust/asserts/src/main.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+
+fn main() {
+    // let mut v: Vec<i32> = Vec::new();
+    // let v1 = vec![1, 2, 3];
+    // 添加元素
+    // v.push(2);
+    // v.push(3);
+    // v.push(4);
+    // 删除元素
+    // 访问元素
+    // let third = v[2];
+    // let second: &i32 = &v[1];
+    // println!("{}", third);
+    // println!("{}", second);
+
+    // if let Some(second) = v.get(1) {
+    //     println!("{}", second)
+    // }
+    // println!("{}", third);
+
+    // fn count(num: i32) {
+    //     println!("{}", num);
+    // }
+
+    // fn countRef(numRef: &i32) {
+    //     println!("{}", numRef);
+    // }
+    // count(third);
+    // countRef(second);
+    // println!("{}", third);
+    // println!("{}", second);
+    // let v = vec![1, 2, 3, 4, 5];
+    // for i in &v {
+    //     println!("{}", i);
+    // }
+
+    // let mut s = String::from("s");
+    // let mut a = "s".to_string();
+    // a.push_str("s");
+    // println!("{}", a);
+    // a.push('s');
+    // let sa = a + "s";
+    // let s = "22";
+    // let s = String::from("陈家两");
+    // println!("{}", s.len());
+    // let s1 = &s[0..6];
+    // println!("{}", s1)
+    let mut hp = HashMap::new();
+    hp.insert(String::from("name"), 32);
+}
