76e55bb693bc77c113c8efaab9d5270fd1e4533a
jaylen.chan <jaylen.chan@hotmail.com>
2022-02-02 11:39:55 +0800

    code

--- code/basic/rust/chapter1/src/main.rs
+++ code/basic/rust/chapter1/src/main.rs
@@ -1,47 +1,88 @@
+#[derive(PartialEq, Debug)]
+enum Age {
+    NewCar,
+    UsedCar,
+}
+
+#[derive(PartialEq, Debug)]
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+#[derive(PartialEq, Debug)]
+struct Car {
+    color: String,
+    motor: Transmission,
+    roof: bool,
+    age: (Age, u32),
+}
+
+fn car_quality(miles: u32) -> (Age, u32) {
+    if miles > 0 {
+        (Age::NewCar, 0)
+    } else {
+        (Age::UsedCar, 0)
+    }
+}
+
+fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
+    let quality = car_quality(miles);
+
+    if quality == (Age::NewCar, 0) {
+        if roof == true {
+            println!(
+                "Prepare a used car: {:?}, {}, Hard top, {} miles\n",
+                motor, color, miles
+            );
+        }
+        Car {
+            color: color,
+            motor: motor,
+            roof: roof,
+            age: car_quality(miles),
+        }
+    } else {
+        Car {
+            color: color,
+            motor: motor,
+            roof: roof,
+            age: car_quality(miles),
+        }
+    }
+}
+
 fn main() {
-    // let days = [
-    //     "Sunday",
-    //     "Monday",
-    //     "Tuesday",
-    //     "Wednesday",
-    //     "Thursday",
-    //     "Friday",
-    //     "Saturday",
-    // ];
-    // let bytes = [0; 5];
+    let colors = ["blue", "green", "red", "silver"];
+    let mut car = Car {
+        color: String::from("red"),
+        motor: Transmission::Automatic,
+        roof: true,
+        age: (Age::NewCar, 10),
+    };
+    let mut engine = Transmission::Manual;
 
-    // println!("days => {:#?}", days);
-    // println!("bytes => {:#?}", bytes);
+    // Car order #1: New, Manual, Hard top
+    car = car_factory(colors[0].to_string(), engine, true, 0);
+    println!(
+        "Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 
-    // let first = days[0];
-    // let seconde = days[1];
+    // Car order #2: Used, Semi-automatic, Convertible
+    engine = Transmission::SemiAuto;
+    car = car_factory(colors[1].to_string(), engine, false, 100);
+    println!(
+        "Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 
-    // println!("first => {}", first);
-    // println!("second => {}", seconde);
-
-    // will panic!!!
-    // let seventh = days[7];
-    // println!("seventh => {}", seventh);
-
-    let three_nums = vec![1, 2, 3, 4, 5];
-    println!("three_nums => {:?}", three_nums);
-
-    let zeroes = vec![0; 5];
-    println!("zeroes => {:?}", zeroes);
-
-    let mut fruit = Vec::new();
-
-    fruit.push("apple");
-    fruit.push("banana");
-    fruit.push("cherry");
-    println!("fruit => {:?}", fruit);
-
-    let a = fruit.pop();
-    let b = fruit.pop();
-    let c = fruit.pop();
-    let d = fruit.pop();
-    println!("{:?}", a);
-    println!("{:?}", b);
-    println!("{:?}", c);
-    println!("{:?}", d);
+    // Car order #3: Used, Automatic, Hard top
+    engine = Transmission::Automatic;
+    car = car_factory(colors[2].to_string(), engine, true, 200);
+    println!(
+        "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 }
