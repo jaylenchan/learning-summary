135704d489405949ea8436741941c43af1aaee3c
jaylen.chan <jaylen.chan@hotmail.com>
2022-02-05 14:06:48 +0800

    code

--- code/basic/rust/mgrep/src/main.rs
+++ code/basic/rust/mgrep/src/main.rs
@@ -5,13 +5,9 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // 为啥不直接args[1]取值
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
     if let Err(e) = mgrep::run(config) {
-        println!("Application error: {}", e);
         process::exit(1);
     }
 }
