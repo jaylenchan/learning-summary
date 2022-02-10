4f2449af2e9e48a47982f5ef5d5ca733830ab1f0
jaylen.work <jaylen.work@hotmail.com>
2022-02-10 11:12:09 +0800

    feat(jest): jest-vue

--- code/develop/fe-test/dom.test.js
+++ code/develop/fe-test/dom.test.js
@@ -2,6 +2,7 @@ import addDivToBody from './dom'
 import $ from 'jquery'
 
 test('测试addDivToBody', () => {
-  // addDivToBody()
-  // expect($('body').find('div').length).toBe(1)
+  addDivToBody()
+  console.log($('body').find('div').length)
+  expect($('body').find('div').length).toBe(0)
 })
