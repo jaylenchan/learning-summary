89bb5d1d817502d9bdef7deb90c65c3e3647954e
jaylen.work <jaylen.work@hotmail.com>
2022-03-12 13:16:11 +0800

    chore(project): delete jest

--- code/develop/jest/test/Strings.test.ts
+++
@@ -1,9 +0,0 @@
-describe('Strings', () => {
-  test('there is no I in team', () => {
-    expect('team').not.toMatch(/I/)
-  })
-
-  test('there is a "stop" in Christoph', () => {
-    expect('Christoph').toMatch(/stop/)
-  })
-})
